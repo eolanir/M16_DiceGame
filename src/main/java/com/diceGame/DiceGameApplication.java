f11cd51d557ba14f12cc98d10925be5fef7cb0b1
Julio Altisent <eolanir@gmail.com>
2021-10-10 20:55:34 +0200

    JWT Auth config

--- src/main/java/com/diceGame/DiceGameApplication.java
+++ src/main/java/com/diceGame/DiceGameApplication.java
@@ -2,6 +2,14 @@ package com.diceGame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.diceGame.security.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class DiceGameApplication {
@@ -10,4 +18,18 @@ public class DiceGameApplication {
 		SpringApplication.run(DiceGameApplication.class, args);
 	}
 
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception{
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(),
+							UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/users").permitAll()
+				.anyRequest().authenticated();
+		}
+	}
 }
